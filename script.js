ed6d02e48dc6f1ca6f71b0488933da2ab6642739
Mohamed Said <ms277712@gmail.com>
2022-09-16 08:50:40 +0200

    first

---
+++ script.js
@@ -0,0 +1,22 @@
+const sideMenu = document.querySelector("aside");
+const menuBtn = document.querySelector("#menu-btn");
+const closeBtn = document.querySelector("#close-btn");
+const themeToggler = document.querySelector(".theme-toggler");
+
+// MObile nav bar show and close--------------
+menuBtn.addEventListener("click", () => {
+  sideMenu.style.display = "block";
+});
+
+closeBtn.addEventListener("click", () => {
+  sideMenu.style.display = "none";
+});
+
+//---------------change theme------------
+themeToggler.addEventListener("click", () => {
+  // change colors
+  document.body.classList.toggle("dark-theme-variables");
+  // change background of theme icon
+  themeToggler.querySelector("span:nth-child(1)").classList.toggle("active");
+  themeToggler.querySelector("span:nth-child(2)").classList.toggle("active");
+});
